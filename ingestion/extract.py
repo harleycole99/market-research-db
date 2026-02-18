e19f7e58d3816cfc519bbca10be2baa594411a49
Harley Cole <harleycole@Harleys-MacBook-Air.local>
2026-02-18 13:33:19 +0000

    feat: added message to acknolwedge form completion

--- ingestion/extract.py
+++ ingestion/extract.py
@@ -4,3 +4,6 @@ with st.form("details"):
     name = st.text_input("Name")
     surname = st.text_input("Surname")
     submitted = st.form_submit_button("submit")
+
+if submitted:
+    st.write("Hello {name} {surname}")
